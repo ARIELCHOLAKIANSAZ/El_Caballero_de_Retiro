ff6deda2b4ec238caf19386a121f76e002d140c9
PogEscopeta <arielcholakian@gmail.com>
2024-07-12 20:50:02 -0300

    Solved the issues left with the weapon
    It now goes back to position normally

--- ElCaballero.gd
+++ ElCaballero.gd
@@ -3,9 +3,9 @@ extends Node3D
 
 @onready var camera = $Body/POV
 @onready var character = $Body
-@onready var weapon = $yPivot/xPivot/Weapon
-@onready var xpivot = $yPivot/xPivot
-@onready var ypivot = $yPivot
+@onready var weapon = $Body/POV/yPivot/xPivot/Weapon
+@onready var xpivot = $Body/POV/yPivot/xPivot
+@onready var ypivot = $Body/POV/yPivot
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var rotation_speed : float = 0.005
@@ -21,42 +21,31 @@ func _unhandled_input(event):
 			xpivot.rotate_x(-event.relative.y * rotation_speed)
 			ypivot.rotate_y(-event.relative.x * rotation_speed)
 			xpivot.rotation.x = clamp(xpivot.rotation.x, deg_to_rad(-30),deg_to_rad(30)) 
-			ypivot.rotation.y = clamp(ypivot.rotation.y, deg_to_rad(get_min_max_y_from_dir(-50)),deg_to_rad(get_min_max_y_from_dir(+50))) 
+			ypivot.rotation.y = clamp(ypivot.rotation.y, deg_to_rad(-50),deg_to_rad(50)) 
 		else:
 			camera.rotate_x(-event.relative.y * rotation_speed)
 			character.rotate_y(-event.relative.x * rotation_speed)
 			
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-func get_min_max_y_from_dir(min_mod):
-	if(min_mod + character.rotation.y < -PI):
-		return(PI+min_mod + character.rotation.y+PI)
-	elif(min_mod + character.rotation.y > PI):
-		return(-(PI-(min_mod+character.rotation.y-PI)))
-	else:
-		return(min_mod + character.rotation.y)
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _process(delta):
+func _process(_delta):
 	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
 		if(is_using_weapon == false):
 			is_using_weapon = true
 			xpivot.rotation_degrees = Vector3(0,0,0)
 			ypivot.rotation_degrees = Vector3(0,0,0)
-			#weapon.rotation_degrees = Vector3(-90,0,0)
-			
+			weapon.rotation_degrees = Vector3(-90,0,0)
 	else:
 		if(is_using_weapon == true):
 			is_using_weapon = false
 			xpivot.rotation_degrees = Vector3(0,0,0)
 			ypivot.rotation_degrees = Vector3(0,-30,0)
-			#weapon.rotation_degrees = Vector3(-15,0,0)
+			weapon.rotation_degrees = Vector3(-15,0,0)
 		is_using_weapon = false
-		ypivot.rotation = character.rotation
-		print(ypivot.rotation)
-		print(character.rotation)
-		xpivot.rotation = camera.rotation
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -72,12 +61,11 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		character.velocity.x = character.direction.x * SPEED
-		character.velocity.z = character.direction.z * SPEED
+		character.velocity.x = character.rotation.x * SPEED
+		character.velocity.z = character.rotation.z * SPEED
 	else:
 		character.velocity.x = move_toward(character.velocity.x, 0, SPEED)
 		character.velocity.z = move_toward(character.velocity.z, 0, SPEED)
 	
-	ypivot.position = character.position
 	
 	character.move_and_slide()
